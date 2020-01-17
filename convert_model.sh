5ae15b57565564de6fa148175d27aa1661974fdb
Giovanni Terlingen <me@giovanniterlingen.com>
2020-01-17 20:15:27 +0100

    Revert "Build coremltools from source"
    This reverts commit 0696edbc90774d46817850b069769bc616a9aa84.

--- convert_model.sh
+++ convert_model.sh
@@ -3,11 +3,10 @@
 brew install python@2
 sudo pip install setuptools
 
+sudo pip install coremltools==3.1
 sudo pip install tensorflow==2.0.0
 sudo pip install tqdm
 sudo pip install sympy
-
-./install_coremltools.sh
 
 python ./download.py
 python ./convert.py
