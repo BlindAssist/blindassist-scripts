cc4048ca0e6d34be50e5b16dc36afea39e01cac6
Giovanni Terlingen <me@giovanniterlingen.com>
2020-01-19 19:50:45 +0100

    Update to coremltools 3.2

--- convert_model.sh
+++ convert_model.sh
@@ -3,7 +3,7 @@
 brew install python@2
 sudo pip install setuptools
 
-sudo pip install coremltools==3.1
+sudo pip install coremltools==3.2
 sudo pip install tensorflow==2.0.0
 sudo pip install tqdm
 sudo pip install sympy
